d77a092e3ae4bbcc5fedb9ed5998987e04d59741
Dominik Kolodziej <kolodziej.dominik@gmail.com>
2018-07-03 11:00:57 +0200

    Start new project - image gallery created using React

---
+++ script.js
@@ -0,0 +1,16 @@
+var GalleryItem = React.createClass({
+    propTypes: {
+        image: React.PropTypes.object.isRequired,
+    },
+    render: function() {
+        return (
+            React.createElement('div', {},
+                React.createElement('h2', {}, this.props.image.name),
+                React.createElement('img', { src: this.props.image.src })
+            )
+        )
+    },
+});
+
+var element = React.createElement(GalleryItem);
+ReactDOM.render(element, document.getElementById('app'));
