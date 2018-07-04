2e93a3556ee89fe6fb6b1f2a2fb90eb9fe1e0ba8
Dominik Kolodziej <kolodziej.dominik@gmail.com>
2018-07-04 10:34:41 +0200

    Add "movies" array from previous project

--- script.js
+++ script.js
@@ -20,6 +20,31 @@ var image = {
 var element = React.createElement(GalleryItem, { image: image });
 ReactDOM.render(element, document.getElementById('app'));
 */
+var movies = [{
+        id: 1,
+        title: 'Harry Potter',
+        desc: 'film o czarodzieju',
+        image: './images/harry.jpg'
+    },
+    {
+        id: 2,
+        title: 'Król lew',
+        desc: 'Film o królu sawanny',
+        image: './images/lion.jpg'
+    },
+    {
+    	id: 3,
+    	title: 'Władca pierścieni',
+    	desc: 'Film fantasy o zmaganiach dobra ze złem w świecie stworzonym przez J.R.R. Tolkiena',
+    	image: './images/gandalf.jpg'
+    },
+    {
+    	id: 4,
+    	title: 'Przyjaciele',
+    	desc: 'Serial komediowy opisujący życie szóstki przyjaciół żyjących w Nowym Jorku',
+    	image: './images/friends.jpg'
+    }
+];
 
 var Movie = React.createClass({
 	propTypes: {
@@ -27,10 +52,10 @@ var Movie = React.createClass({
 	},
     render: function() {
         return (
-            React.createElement('li', { key: movie.id },
-                React.createElement('h2', {}, movie.title),
-                React.createElement('p', {}, movie.desc),
-                React.createElement('img', { src: movie.image })
+            React.createElement('li', { key: this.props.movie.id }),
+                React.createElement(MovieTitle, {MovieTitle: this.props.movie.title)},
+                React.createElement(MovieDescription, {MovieDescription: this.props.movie.desc}),
+                React.createElement('img', { src: this.props.movie.image })
             )
         )
     },
